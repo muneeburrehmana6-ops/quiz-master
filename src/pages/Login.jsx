9df9899d8294d7a0315b90fdf05b9303290e0e04
Muneeb ur Rehman <muneeburrehmana6@gmail.com>
2026-08-05 03:16:28 -0700

    Fix Login.css import

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import "../styles/login.css";
+import "../styles/Login.css";
 
 function Login() {
   const [username, setUsername] = useState("");
