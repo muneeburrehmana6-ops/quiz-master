8062654f80b58b637507bfa5c045a0914e54e4f0
Muneeb ur Rehman <muneeburrehmana6@gmail.com>
2026-08-08 13:20:11 -0700

    Add SEO metadata to About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,15 +5,42 @@ function About() {
   return (
     <>
       <Helmet>
-        <title>About Us | QuizMaster</title>
+        <title>About QuizMaster Learn - Online Learning & Quizzes</title>
+
         <meta
           name="description"
-          content="Learn more about QuizMaster and our mission to make programming learning easier."
+          content="Learn more about QuizMaster Learn, an online educational platform offering interactive quizzes for programming, web development, science, mathematics, English and general knowledge."
         />
+
+        <meta
+          name="keywords"
+          content="about QuizMaster Learn, online quizzes, educational quizzes, programming quizzes, learning platform, quiz platform"
+        />
+
+        <link
+          rel="canonical"
+          href="https://www.quizmasterlearn.com/about"
+        />
+
+        <meta
+          property="og:title"
+          content="About QuizMaster Learn - Online Learning & Quizzes"
+        />
+
+        <meta
+          property="og:description"
+          content="Learn more about QuizMaster Learn and our mission to make learning engaging through interactive online quizzes."
+        />
+
+        <meta
+          property="og:url"
+          content="https://www.quizmasterlearn.com/about"
+        />
+
+        <meta property="og:type" content="website" />
       </Helmet>
 
       <div className="about-page">
-
         <div className="about-container">
 
           <h1>About QuizMaster</h1>
@@ -64,7 +91,6 @@ function About() {
           </div>
 
         </div>
-
       </div>
     </>
   );
